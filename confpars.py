f3ee8dc8400e1d839f406d951b35d61bc0b23e0a
QSST <84291525+qust1n@users.noreply.github.com>
2022-05-07 05:24:12 +0300

    Add files via upload

--- confpars.py
+++ confpars.py
@@ -1,7 +1,13 @@
-import configparser
-config = configparser.ConfigParser()
-config['pyrogram'] = {'api_id': int(input('Enter your api_id: ')),
-                     'api_hash': str(input('Enter your api_hash: '))}
-
-with open('config.ini', 'w') as configfile:
-   config.write(configfile)
+try:
+	api_id = int(input("Enter api_id: "))
+	api_hash = input("Enter api_hash: ")
+	
+	with open('config.py', 'w') as file:
+		file.write("api_id = " + str(api_id)+
+						 "\napi_hash = " + str(api_hash))
+		
+		
+	print("\n\033[32mDone!")
+		
+except ValueError:
+	print("\n\033[31mapi_id consists of numbers only")
